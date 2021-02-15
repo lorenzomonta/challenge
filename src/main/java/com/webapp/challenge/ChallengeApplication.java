869862006fa4a967a76d6b8e597376e210562db9
lorenzomonta <m_lorenzo93@yahoo.it>
2021-02-15 12:58:17 +0100

    second milestone

--- src/main/java/com/webapp/challenge/ChallengeApplication.java
+++ src/main/java/com/webapp/challenge/ChallengeApplication.java
@@ -4,15 +4,22 @@ import check.numbers.CSVReaderInJava;
 import check.numbers.Number;
 import check.numbers.Validator;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
 import java.util.List;
 
+@Configuration
 @SpringBootApplication
 public class ChallengeApplication {
 
-    public static final String PATH = "/Users/lorenzomontanari/Downloads/South_African_Mobile_Numbers.csv";
+    //public static final String PATH = "/Users/lorenzomontanari/Downloads/South_African_Mobile_Numbers.csv";
 
     public static void main(String[] args) {
+        new File(Controller.UPLOADDIRECTORY).mkdir();
         SpringApplication.run(ChallengeApplication.class, args);
         /*CSVReaderInJava csvReader = new CSVReaderInJava();
         List<Number> numbers = csvReader.readBooksFromCSV(PATH);
--- src/main/java/com/webapp/challenge/Controller.java
+++ src/main/java/com/webapp/challenge/Controller.java
@@ -1,14 +1,27 @@
 package com.webapp.challenge;
 
+import check.numbers.CSVReaderInJava;
 import check.numbers.Number;
 import check.numbers.Validator;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    protected static final String UPLOADDIRECTORY = System.getProperty("user.dir") + "/uploads";
+    protected StringBuilder fileNames;
 
     @RequestMapping(value = "/")
     public String index(){
@@ -16,12 +29,43 @@ public class Controller {
     }
 
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @RequestMapping(value = "/validatenumber", method = RequestMethod.POST)
     public String processCheckNumber (@ModelAttribute Number num, Model model){
         Validator v = new Validator(num);
         v.validateNumber();
         model.addAttribute("annotation", v.getNumber().getAnnotation());
         //System.out.println(v.getNumber());
         return "index";
+    }
+
+    @RequestMapping(value = "/validatenumbers")
+    public String upload(Model model, @RequestParam("files") MultipartFile[] files) {
+        fileNames = new StringBuilder();
+        for (MultipartFile file : files) {
+            Path fileNameAndPath = Paths.get(UPLOADDIRECTORY, file.getOriginalFilename());
+            fileNames.append(file.getOriginalFilename());
+            try {
+                Files.write(fileNameAndPath, file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String path = UPLOADDIRECTORY + "/" + fileNames;
+        System.out.println("Verifica : " + path);
+        CSVReaderInJava csvReader = new CSVReaderInJava();
+        List<Number> numbers = csvReader.readBooksFromCSV(path);
+        Validator validator = new Validator(numbers);
+        validator.validateNumbers();
+        /*
+        for (Number i : numbers) {
+            System.out.println(i);
+            //if (i.isCorrect()) System.out.println(i);
+            //if (!i.isCorrect()) System.out.println(i);
+        }
+        */
+        model.addAttribute("msg", "Successfully uploaded files " + fileNames.toString());
+        model.addAttribute("listNumbers", numbers);
+        return "validatenumbers";
     }
 }
